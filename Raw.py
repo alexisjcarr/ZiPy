6c935e3c9067a0e92bcded625ef72cded1fe07fc
malik180 <46081556+malik180@users.noreply.github.com>
2019-04-08 23:19:00 -0500

    Update Raw.py

--- Raw.py
+++ Raw.py
@@ -18,10 +18,10 @@ import scipy as sp
 #Maximum Likelihood function
 
 def ziPoisson(parms):
-        mu= np.vectorize(exp(X @ parms[1:kx]) + offsetx)    #mean. kx is no of columns of X.
-        phi= np.vectorize(*linkinv*(Z @ parms[(kx+1):(kx+kz)] + offsetz)) # need to find linkinv function in python or alternative
+        mu= np.vectorize(exp(X @ parms[1:kx]) + offsetx)   # kx is no of columns of X.vector of non-negative means of the uninflated Poisson distribution
+        phi= np.vectorize(*linkinv*(Z @ parms[(kx+1):(kx+kz)] + offsetz)) #this represents the proportion of excessive zeros presentvector of zero inflation probabilities for structural zeros.
         return sum(*dzipois*(Y, lambda = mu, pi = phi, log = TRUE)) * weights) # find zero inflated poisson distribution function in python
-        
+        #log=TRUEmeans probabilities p are given as log(p) , Y represents vector of (non-negative integer) quantiles(most probably a dummy variable)
         
 #using scipy.optimize to maximize the function 
 from scipy.optimize import minimize
