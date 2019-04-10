0be95ec3905576cc4f4b0c05e66f2a675a4f511c
Alexis J Carr <34029816+alexisjcarr@users.noreply.github.com>
2019-04-09 19:38:22 -0500

    Add files via upload

--- zeroinf.py
+++ zeroinf.py
@@ -1,126 +1,90 @@
 import numpy as np
 import scipy as sp 
 from scipy.special import logit, expit
+import patsy as pat
 
 class Zeroinf:
-    def _init_(self, formula, data, dist= 'poisson', link= 'logit'):
-        ## call and formula
+    def _init_(self,formula, data, dist=’poisson’, link=’logit’):
         '''
-        cl <- match.call()
-        if(missing(data)) data <- environment(formula)
-        mf <- match.call(expand.dots = FALSE)
-        m <- match(c("formula", "data", "subset", "na.action", "weights", "offset"), names(mf), 0)
-        mf <- mf[c(1, m)]
-        mf$drop.unused.levels <- TRUE
+        ***Constructor***
         '''
+        '''
+        The following variables are defined within formula_extraction
 
-        ## extended formula processing
+        self.X # count data model regressors
+        self.Z # zero-inflated model regressors
+        self.Y 
         '''
-        if(length(formula[[3]]) > 1 && identical(formula[[3]][[1]], as.name("|")))
-        {
-            ff <- formula
-            formula[[3]][1] <- call("+")
-            mf$formula <- formula
-            ffc <- . ~ .
-            ffz <- ~ .
-            ffc[[2]] <- ff[[2]]
-            ffc[[3]] <- ff[[3]][[2]]
-            ffz[[3]] <- ff[[3]][[3]]
-            ffz[[2]] <- NULL
-        } else {
-            ffz <- ffc <- ff <- formula
-            ffz[[2]] <- NULL
-        }
-        if(inherits(try(terms(ffz), silent = TRUE), "try-error")) {
-            ffz <- eval(parse(text = sprintf( paste("%s -", deparse(ffc[[2]])), deparse(ffz) )))
-        }
-
-        '''
-        ## call model.frame()
-        '''
-        mf[[1]] <- as.name("model.frame")
-        mf <- eval(mf, parent.frame())
-        '''
-
-        ## extract terms, model matrices, response
-        self.mt = ''
-        self.mtX = ''
-        self.X = ''
-        
-        self.mtZ = ''
-        self.Z = ''
-
-        self.Y = ''
+        self.formula = formula
+        self.data = data
+        self.offsetz, self.offsetx = 0.0
+        self.weights = 1.0
 
         ## convenience variables
-        self.n = len(Y)
+        self.n = len(self.Y)
         self.kx = self.X.shape[1] #number of columns in X matrix
         self.kz = self.Z.shape[1] #number of columns in Z matrix
     	self.Y0 = self.Y <= 0        	
         self.Y1 = self.Y > 0
 
+    def formula_extraction(self):
+        self.X_, self.Z_ = formula.str.split('|')
+        self.Z_ = '{beg}{z_}'.format(beg=formula.str.split('~')[0],z_=Z_)
+        self.Y_, self.X = pat.dmatrices(X_, data, return_type='dataframe')
+        self.Z = pat.dmatrices(Z_, data, return_type='dataframe')[1]
+        self.Y = np.squeeze(self.Y_)
+        
+
     def ziPoisson(self, parms): 
-	'''
-	Log-likelihood for zeroinfl
-		
-        aside: parms a function that gets or sets simulation model parameters, 
-        main or sub-equations, initial values, time steps or solvers and 
-        extract simulation results.
+		'''
+		***Log-likelihood for Zeroinf***
         '''
-	## count mean
-        mu = np.exp(self.X @ parms[1:kx] + offsetx) #offsets will be set in constructor
-	## binary mean
-        phi = expit(self.Z @ parms[(kx+1):(kx+kz)] + offsetz))
-        # vector of linkinv(Z @ parms[(kx+1):(kx+kz)] + offsetz))
-        '''
-        For now: Just hard code the inverse of logit
-        link <- ## whether it's logit etc.
-        linkstr <- match.arg(link)
-            matches string argument to name in list
-            is there a link list in R code? yes.
-            it's in function def 
-            maybe don't need rn TODO
-        linkobj <- make.link(linkstr)
-            turns linkstr into link object
-        linkinv <- linkobj$linkinv
-            extracts inverse link function from link object
-        '''
+		## count mean
+        mu = np.exp(self.X @ parms[1:self.kx] + self.offsetx) 
+		## binary mean
+        phi = expit(self.Z @ parms[(self.kx+1):(self.kx+self.kz)] + self.offsetz))
+        # expit is inverse link of logit
+       
         ## log-likelihood for y = 0 and y >= 1
         loglik0 = np.log( phi + np.exp( log(1-phi) – mu ) ) 
-        loglik1 = np.log(1-phi) + sp.stats.poisson.pmf(self.Y, lambda=mu)
+        loglik1 = np.log(1-phi) + sp.stats.poisson.pmf(Y, lambda=mu)
         ## collect and return
-        loglik = sum(loglik[self.Y0] + loglik[self.Y1])
-        ##loglik <- sum(weights[Y0] * loglik0[Y0]) + sum(weights[Y1] * loglik1[Y1])
-        return loglik # return loglik
+        loglik = sum(self.weights[self.Y0] @ loglik0[self.Y0]) + \
+            sum(self.weights[self.Y1] @ loglik1[self.Y1])
+        return loglik
+
+    def gradPoisson(self, parms):
         '''
-        Above is the dot product, I believe. So you are taking 
-        Σ[weights[Y0] • (loglik0[Y0] + Σ(weights[Y1] • loglik1[Y1])]
-        weights = 1, so code works for now. 
+        ***Gradient likelihood for Zeroinf***
         '''
+		## count mean
+		eta = # X • parms[1:kx} + offsetx
+		mu = np.exp(eta)
+		## binary mean
+		etaz = # z • parms[{kx+1):(kx+kz)] + offsetz)
+		muz = # linkinv(etaz) # will probably need Jones help
 
-    def gradPoisson(self, parms): # will work on tonight
-	## count mean
-	eta = self.X @ parms[1:kx} + offsetx
-	mu = np.exp(eta)
-	## binary mean
-	etaz = self.Z @ parms[{kx+1):(kx+kz)] + offsetz)
-	muz = expit(etaz) 
+		## densities at 0
+		clogdens0 = -mu
+		dens0 = muz * (1 – Y1) + np.exp(np.log(1-muz) + clogdens0)
 
-	## densities at 0
-	clogdens0 = -mu
-	dens0 = muz * (1 – self.Y1) + np.exp(np.log(1-muz) + clogdens0)
+		## working residuals
+		if(Y1):
+			wres_count = Y – mu
+		else:
+			wres_count = -np.exp(-np.log(dens0) + log(1-muz) +\ 
+                clogdens0 + log(mu)))
 
-	## working residuals
-	if(self.Y1): #boolean truthy
-		wres_count = self.Y – mu
-	else:
-		wres_count = -np.exp(-np.log(dens0) + log(1-muz) + clogdens0 + log(mu)))
-		
-	if(self.Y1):
-		wres_zero = -1/(1-muz)* #(linkobk$mu.eta(etaz)) -- Ask Jones
-	else:
-		wres_zero = #(linkobj$mu.eta(etaz) – np.exp(clogdens0) * linkobj$mu.eta(etaz))/dens0) --AJ
-		
-	return # column sums of these two columns bound
+		if(Y1):
+			wres_zero = -1/(1-muz)* #(linkobk$mu.eta(etaz))
+		else:
+			wres_zero = #(linkobj$mu.eta(etaz) – np.exp(clogdens0) * linkobj$mu.eta(etaz))/dens0) # looking for def for that
+
+		return # column sums of these two columns bound
 			# 1) wres_count * weights * X
 			# 2) wres_zero * weights * Z
+
+    def __repr__(self):
+        '''
+        part of the print function will go here
+        '''
