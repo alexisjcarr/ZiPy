a3dd5c532e6820312fcc973ceaedddc106d523c1
Alexis J Carr <34029816+alexisjcarr@users.noreply.github.com>
2019-04-11 20:26:44 -0500

    almost functional

--- zeroinf.py
+++ zeroinf.py
@@ -3,11 +3,13 @@ import scipy as sp
 import pandas as pd
 from scipy.special import logit, expit
 import patsy as pat
+import statsmodels.api as sm
+from statsmodels.genmod.families import Poisson, Binomial
 
 class Zeroinf:
     def __init__(self, formula, data, dist='poisson', link='logit'):
         '''
-        ***Constructor***
+        Class Constructor
         '''
         self.formula = formula
         self.data = data
@@ -21,6 +23,8 @@ class Zeroinf:
         self.kz = self.Z.shape[1] #number of columns in Z matrix
         self.Y0 = self.Y <= 0  
         self.Y1 = self.Y > 0
+        self.theta = NULL
+        self.log_theta = NULL
         
         ## offsets and weights
         self.offsetz = np.repeat(0, self.n)
@@ -39,7 +43,7 @@ class Zeroinf:
     
     def ziPoisson(self, parms): # parms supplied by self.mlEstimation.
         '''
-        ***Log-likelihood for Zeroinf***
+        Log-likelihood for ZIP Model
         '''
         ## count mean
         mu = np.exp(self.X @ parms[1:self.kx] + self.offsetx) 
@@ -58,7 +62,7 @@ class Zeroinf:
 
     def gradPoisson(self, parms):
         '''
-        ***Gradient likelihood for Zeroinf***
+        Gradient function for ZIP Model
         '''
         ## count mean
         eta = self.X @ parms[1:kx] + self.offsetx
@@ -93,18 +97,90 @@ class Zeroinf:
 
     def startingValues(self):
         '''
-        will return count model and binomial model from glm
+        Returns count model and binomial model from glm
         '''
+        modelCount = sm.GLM(endog = self.Y, exog = self.X, family = Poisson(),\
+             weights = self.weights, offset = self.offsetx).fit()
+        modelZero = sm.GLM(endog = self.Y0.astype(int), exog = self.Z,\
+            family = Binomial(link=logit), weights = self.weights,\
+                 offset = self.offsetz).fit()
 
-        return self.start_values
+        self.startValues =  {'zeroStartValues': modelZero.params,\
+            'countStartValues': modelCount.params}
+
+        return self.startValues
 
     def mlEstimation(self):
+        fun = self.ziPoisson
+        x0 = self.startValues # TODO (extracted)
+        method = 'BFGS' #(per paper)
+        jac = self.gradPoisson
+        options = {'maxiter': 10000, 'disp': True}
+
+        self.fit = sp.optimize.minimize(fun = fun, x0 = x0, method = method, jac = jac,\
+             options = options) #returns object OptimizeResult
+
+        ## coefficients and covariances
         '''
-        scipy.optimize.minimize stuff
+        coefc <- fit$par[1:kx]
+        names(coefc) <- names(start$count) <- colnames(X)
+        coefz <- fit$par[(kx+1):(kx+kz)]
+        names(coefz) <- names(start$zero) <- colnames(Z)
+
+        colnames(vc) <- rownames(vc) <- c(paste("count", colnames(X), sep = "_"), \
+            paste("zero",  colnames(Z), sep = "_"))
         '''
-        return ''
+
+        ## fitted and residuals
+        '''
+        mu <- exp(X %*% coefc + offsetx)[,1]
+        phi <- linkinv(Z %*% coefz + offsetz)[,1]
+        Yhat <- (1-phi) * mu
+        res <- sqrt(weights) * (Y - Yhat)
+        '''
+
+        ## effective observations
+        '''
+        nobs <- sum(weights > 0) ## = n - sum(weights == 0)
+
+        rval <- list(coefficients = list(count = coefc, zero = coefz),
+            residuals = res,
+            fitted.values = Yhat,
+            optim = fit,
+            method = method,
+            control = ocontrol,
+            start = start,
+            weights = if(identical(as.vector(weights), rep.int(1L, n))) NULL else weights,
+            offset = list(count = if(identical(offsetx, rep.int(0, n))) NULL else offsetx,
+            zero = if(identical(offsetz, rep.int(0, n))) NULL else offsetz),
+            n = nobs,
+            df.null = nobs - 2,
+            df.residual = nobs - (kx + kz + (dist == "negbin")),
+            terms = list(count = mtX, zero = mtZ, full = mt),
+            theta = theta,
+            SE.logtheta = SE.logtheta,
+            loglik = fit$value,
+            vcov = vc,
+            dist = dist,
+            link = linkstr,
+            linkinv = linkinv,
+            converged = fit$convergence < 1,
+            call = cl,
+            formula = ff,
+            levels = .getXlevels(mt, mf),
+            contrasts = list(count = attr(X, "contrasts"), zero = attr(Z, "contrasts"))
+        )
+        if(model) rval$model <- mf
+        if(y) rval$y <- Y
+        if(x) rval$x <- list(count = X, zero = Z)
+            
+        class(rval) <- "zeroinfl"
+        return(rval)
+        }
+        '''
+        return {} # return the hell out of that dictionary
 
     def __repr__(self):
         '''
-        change as needed for testing
+        Change as needed for testing
         '''
