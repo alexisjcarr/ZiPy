56cdff5d8206e4185af2d1262a75a08444a64f6f
Alexis J Carr <34029816+alexisjcarr@users.noreply.github.com>
2019-04-30 21:39:22 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name=‘zipy’,
+setup(name=‘ZiPy’,
       version='0.1',
       description=‘Python package for maximum likelihood estimation of zero-inflated models for count data.’,
       url='https://github.com/alexisjcarr/ZiPy’,
       author=‘Alexis J. Carr’,
       author_email=‘acarr59@gmail.com’,
       license='MIT',
-      packages=[‘zipy’],
+      packages=[‘ZiPy’],
       zip_safe=False)
